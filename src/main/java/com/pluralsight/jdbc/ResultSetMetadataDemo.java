96bae4a7aca75b74da170c0f04ed02fd416ed671
Nik Gibbens <ngibbens@hotmail.com>
2017-01-18 08:52:08 -0800

    Pluralsight training for JDBC: ResultSetMetadata

--- src/main/java/com/pluralsight/jdbc/ResultSetMetadataDemo.java
+++ src/main/java/com/pluralsight/jdbc/ResultSetMetadataDemo.java
@@ -1,7 +1,35 @@
 package com.pluralsight.jdbc;
 
+import java.sql.*;
+
 /**
  * Created by Nik on 1/18/17.
  */
 public class ResultSetMetadataDemo {
+
+    public static void main(String[] args) {
+
+        try(Connection conn = DBUtil.getConnection(DBType.ORADB);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from NewEmployees");
+            ) {
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+
+            String format = "%-50s%-25s%-20s%-20s\n";
+            System.out.format(format, "Column Name", "Column Type", "Is Nullable", "Is AutoIncrement");
+            System.out.format(format, "----------", "----------", "----------", "----------");
+            for(int i = 1; i <= columnCount; i++) {
+                System.out.format(format, rsmd.getColumnName(i), rsmd.getColumnType(i), rsmd.isNullable(i), rsmd.isAutoIncrement(i));
+            }
+
+            System.out.println("Total Columns : " + columnCount);
+
+        }
+        catch(SQLException ex) {
+            DBUtil.showErrorMessage(ex);
+        }
+
+    }
 }
